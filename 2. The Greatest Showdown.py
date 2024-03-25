5a8cf67a9a0fb1181f3b17ca00cc74df8340ab66
Marko <markogidej@yahoo.com>
2024-03-25 16:45:14 -0400

    Changed answer for Question 2 accounting for last task

--- 2. The Greatest Showdown.py
+++ 2. The Greatest Showdown.py
@@ -6,30 +6,37 @@ val_1 = input()
 print("Enter second number:")
 val_2 = input()
 
-print("Enter thrid number:")
+print("Enter third number:")
 val_3 = input()
 
-if val_1 > val_2 and val_1 > val_3:
-    print("Largest number from inputs is: " + str(val_1))
-elif val_2 > val_1 and val_2 > val_3:
-    print("Largest number from inputs is: " + str(val_2))
-elif val_3 > val_1 and val_3 > val_2:
-    print("Largest number from inputs is: " + str(val_3))
-else:
-    print("No single largest number!")
+largest_value = val_1
+
+if largest_value < val_2:
+    largest_value = val_2
+
+if largest_value < val_3:
+    largest_value = val_3
+
+print("Largest number from inputs is: " + str(largest_value))
 
 
 # Task 2: Identify the Smallest
+smallest_value = val_1
 
-if val_1 < val_2 and val_1 < val_3:
-    print("Smallest number from inputs is: " + str(val_1))
-elif val_2 < val_1 and val_2 < val_3:
-    print("Smallest number from inputs is: " + str(val_2))
-elif val_3 < val_1 and val_3 < val_2:
-    print("Smallest number from inputs is: " + str(val_3))
-else:
-    print("No single smallest number!")
+if smallest_value > val_2:
+    smallest_value = val_2
+
+if smallest_value > val_3:
+    smallest_value = val_3
+
+print("Smallest number from inputs is: " + str(smallest_value))
 
 
 # Task 3: Equality Check
 
+if val_1 == val_2 == val_3:
+    print("All numbers are equal")
+elif val_1 == val_2 or val_1 == val_3:
+    print("Two numbers are equal, this is: " + str(val_1))
+elif val_2 == val_3:
+    print("Two numbers are equal, this is: " + str(val_2))
