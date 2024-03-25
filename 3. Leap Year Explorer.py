9394599b6fd1e816efa3bf8a41c163d69151284a
Marko <markogidej@yahoo.com>
2024-03-25 16:58:44 -0400

    Answered Question 3 all tasks

--- 3. Leap Year Explorer.py
+++ 3. Leap Year Explorer.py
@@ -1,1 +1,14 @@
 # Task 1
+
+print("Please enter a year:")
+year = int(input())
+
+def is_leap_year(input_year):
+    if input_year % 400 == 0:
+        return True;
+    elif (input_year % 4 == 0) and (input_year % 100 != 0):
+        return True;
+    else:
+        return False
+
+print(str(is_leap_year(year)))
